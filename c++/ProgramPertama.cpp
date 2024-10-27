c0cef0c4704b42a98f5e4480fd8ecf1133503049
rizzfar <alfaridrizky69@gmail.com>
2024-10-27 19:58:44 +0700

    add some files

---
+++ c++/ProgramPertama.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Halo Nama Ku Rizky Al Farid" << endl;
+  cout << "Sekarang Aku Kuliah Di Universitas Komputer Indonesia" << endl;
+  return 0;
+}
---
+++ c++/program_output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "=========== PROGRAM OUTPUT ===========" << endl;
+  cout << "Program ini adalah program untuk\n";
+  cout << "Menampilkan output ke layar komputer.\n";
+  cout << "Output sangat penting buat menampilkan \n";
+  cout << "Informasi kepada user.\n";
+  cout << "======================================" << endl;
+
+  return 0;
+}
